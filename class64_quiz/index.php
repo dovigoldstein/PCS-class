e3fe798b1608e85d1d7acd4734e5fe17076468ae
Dovi Goldstein <dovigoldstein@gmail.com>
2017-07-16 22:22:21 -0400

    Quiz

---
+++ class64_quiz/index.php
@@ -0,0 +1,84 @@
+<?php
+    try {
+        $db = new PDO("mysql:host=localhost;dbname=students", "user", 'password',[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+        $query = "SELECT a.name,a.score as score1, b.score as score2
+                FROM tests A, tests B
+                WHERE a.score <> b.score
+                AND a.name = b.name
+                GROUP BY a.name";
+                $statement = $db->prepare($query);
+                $statement->execute();
+                $students = $statement->fetchall(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            $systemError = "ERROR: " . $e->getMessage();
+        }
+        // print_r($students);
+        if(! empty($_POST['student'])){
+            try {
+                $delete = "DELETE * FROM 'tests' WHERE 'name' = ? ";
+                $statement = $db->prepare($delete);
+                $statement->bindValue(1,$_POST['student']);
+                $statement->execute();
+                } catch (PDOException $e) {
+                    $systemError = "ERROR: " . $e->getMessage();
+                }
+        }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="jumbotron text-center">
+            <h1>Quiz</h1>
+        </div>
+        <table class="table table-striped table-hover">
+            <caption>
+                <h3 class="text-center">Student Test Scores</h3>
+            </caption>
+            <thead>
+                <tr>
+                    <th>Select 1</th>
+                    <th>Student</th>
+                    <th>Grade 1</th>
+                    <th>Grade 2</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($students as $student) : ?>
+                <form method="post">
+                    <tr>
+                        <td><input type="radio" name="student" value="<?=$student['name'] ?>" ></td>
+                        <td><?=$student['name']?></td>
+                        <td><?=$student['score1']?></td>
+                        <td><?=$student['score2']?></td>
+                    </tr>
+                
+                <?php endforeach ?>
+                <tfoot>
+                    <tr>
+                        <td><button type="submit" class="btn btn-danger">Delete</button></td>
+                    </tr>
+                </tfoot>
+                <form >
+
+            </tbody>
+        </table>
+        <?php if(!empty($systemError)) : ?>
+                    <span class="text-danger">
+                        <?= $systemError?>
+                <?php endif ?>
+                </span>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
