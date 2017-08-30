33d9bee3a1a5539c0d58d0fe546dd92c709ddf83
Dovi Goldstein <dovigoldstein@gmail.com>
2017-08-29 21:14:30 -0400

    added HW_70

---
+++ HW_70/index.php
@@ -0,0 +1,30 @@
+<?php
+if(!empty($_COOKIE["TimeDateCookie"])){
+    $date = date('m/d/Y h:i:s a', $_COOKIE["TimeDateCookie"]);
+    $message = "Welcome back!<br/>Last Visit: $date";
+}else{
+    $message = "Welcome to our site!";
+}
+setCookie("TimeDateCookie", time(), time() + (60 * 60 * 24 * 365));
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="jumbotron">
+        <h1 class = "text-center"><?= $message ?></h2>
+    </div>
+
+    
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+  </body>
+</html>
