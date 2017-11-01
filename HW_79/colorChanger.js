034473659c2d35cdd2a41da0929f6e4af60b8139
Dovi Goldstein <dovigoldstein@gmail.com>
2017-11-01 13:08:11 -0400

    minor change

--- HW_79/colorChanger.js
+++ HW_79/colorChanger.js
@@ -1,4 +1,5 @@
 (function(){
+    "use strict";
     document.getElementById('submit').addEventListener('click', function () {
         document.body.style.backgroundColor = document.getElementById('backgroundColor').value;
         document.body.style.color = document.getElementById('fontColor').value;
