a6470de6f50eb902a89953bef7202f88a6a42f14
Dovi Goldstein <dovigoldstein@gmail.com>
2017-12-24 18:48:19 -0500

    redone without bootstrap js

--- HW_89/imageGallery.js
+++ HW_89/imageGallery.js
@@ -1,31 +1,56 @@
 /*global $ */
 (function () {
     "use strict";
-
-    var myCarousel = $("#myCarousel");
-    var carouselPics = $('#carouselPics');
+    var theImage = $('#theImage');
+    var theCaption = $('#theCaption');
+    var prev = $('#prev');
+    var next = $('#next');
     var playPause = $('#playPause');
     var playPauseImg = $('#playPause img');
+    var intervalId;
+    var images = [];
+    var imageIndex = 0;
+
+    function setSlide() {
+        theImage.attr('src', images[imageIndex].url);
+        theCaption.html(images[imageIndex].description);
+    }
+
+    function getNext() {
+        imageIndex++;
+        if (imageIndex >= images.length) {
+            imageIndex = 0;
+        }
+        setSlide();
+    }
+
+    function getPrev() {
+        imageIndex--;
+        if (imageIndex < 0) {
+            imageIndex = images.length - 1;
+        }
+        setSlide();
+    }
 
     $.getJSON('images.json', function (loadedImages) {
         loadedImages.forEach(function (element) {
-            carouselPics.append('<div class="carousel-item">' +
-                '<img class="d-block w-100" src="' + element.url + '" alt="">' +
-                '<div class="carousel-caption d-none d-md-block">' +
-                '<h3>' + element.description + '</h3></div></div>');
+            images.push(element);
         });
-        $(".carousel-item:first-of-type").addClass("active");
-        myCarousel.carousel('pause');
+        setSlide();
     }).fail(function (xhr, statusCode, statusText) {
         console.log(xhr, statusCode, statusText);
     });
 
+    prev.click(getPrev);
+
+    next.click(getNext);
+
     playPause.click(function () {
         if (playPauseImg.attr('src') === '../images/play.png') {
-            myCarousel.carousel('cycle');
+            intervalId = setInterval(getNext, 3000);
             playPauseImg.attr('src', '../images/pause.png');
         } else {
-            myCarousel.carousel('pause');
+            clearInterval(intervalId);
             playPauseImg.attr('src', '../images/play.png');
         }
 
